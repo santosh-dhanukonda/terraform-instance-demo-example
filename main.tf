7209063facf1b9f81bbbc8ac4038d20ab65b3353
Santosh Dhanukonda <sdhanukonda@Santoshs-MacBook-Air.local>
2021-09-22 23:49:20 -0400

    Added custom security Group

--- main.tf
+++ main.tf
@@ -3,11 +3,31 @@ provider "aws" {
 	region = "us-east-1"
 }
 
-resource "aws_instance" "ecs-example" {
+resource "aws_instance" "ec2-demo-example" {
 	ami = "ami-087c17d1fe0178315"
 	instance_type = "t2.micro"
+	vpc_security_group_ids = [aws_security_group.webserver_security_group.id]
+	user_data = <<-EOF
+				#!/bin/bash
+				echo "Hello Santosh" > index.html
+				nohup busybox httpd -f -p 8080 &
+				EOF
 	tags= {
-		Name = "terraform-example1"
+		Name = "demo-examples"
 	}
 }
 
+resource "aws_security_group" "webserver_security_group" {
+	name = "Webs servers Group"
+	description = "Host all of the HTTP Webservers"
+	ingress  {
+	      from_port        = 8080
+	      to_port          = 8080
+	      protocol         = "tcp"
+	      cidr_blocks      = ["0.0.0.0/0"]
+	    }
+  	
+  	tags = {
+		Name = "demo-examples"
+	}
+}
